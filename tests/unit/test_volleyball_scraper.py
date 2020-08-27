4ac8ca1a04d861428768fccc36b97c3aeef08c71
Scott <briggzee@gmail.com>
2020-08-27 08:05:45 +0800

    pytest tests working.

--- tests/unit/test_volleyball_scraper.py
+++ tests/unit/test_volleyball_scraper.py
@@ -8,12 +8,12 @@ from volleyball_scraper import VolleyballLadder, VolleyballRoster
 roster_content = Path('tests/data/monday.html').read_text()
 
 
-@patch('volleyball_scraper.requests.get')
+@patch('requests.get')
 def test_Volleyball_ladder(mock_get):
 
     ladder_content = Path('tests/data/monday_ladder.html').read_text()
 
-    mock_get.text = ladder_content
+    mock_get.return_value.text  = ladder_content
     # mock_get.return = 200
 
     ladder = VolleyballLadder("http://ossieindoorbeachvolleyball.com.au/monday-mens")
